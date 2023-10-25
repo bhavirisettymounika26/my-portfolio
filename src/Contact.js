0eb28192b30bed607066b5ea6488f6e51d2927d5
bhavirisettymounika26 <144678728+bhavirisettymounika26@users.noreply.github.com>
2023-10-25 19:28:11 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -4,8 +4,8 @@ const Contact = () => {
   return (
     <div className="section">
       <h2>Contact Me</h2>
-      <p>Email: mounika.bhaviri2021a@vitstudetn.ac.in</p>
-      <p>Phone: 7654893214</p>
+      <p>Email: bhavirisetty.mounika2021@vitstudent.ac.in</p>
+      <p>Phone: 9866361976</p>
       <p>LinkedIn:https://www.linkedin.com/in/mounika-098992247</p>
     </div>
   );
